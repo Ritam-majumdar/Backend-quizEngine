7f0556903c010c2f15896e3e77325bd6cd2bee38
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-08-31 10:53:45 +0530

    First commit

---
+++ QuizServer/Controllers/QuestionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace QuizServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private IHubContext<QuestionHub> _questionHubContext;
+
+        public QuestionController(IHubContext<QuestionHub> questionHubContext)
+        {
+            _questionHubContext = questionHubContext;
+        }
+
+        public IActionResult Post()
+        {
+            //Broadcast message to client  
+            _questionHubContext.Clients.All.SendAsync("send", "This is a question");
+
+            return Ok();
+        }
+    }
+}
---
+++ QuizServer/QuestionHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizServer
+{
+    public class QuestionHub : Hub
+    {
+        public Task Send(string question)
+        {
+            Console.WriteLine("This is the", question);
+            return Clients.All.SendAsync("Send", question);
+        }
+    }
+}
